7a7920f0337c28e71c8527c849f78fbc541565a9
Christoph Dorn <christoph@christophdorn.com>
2015-09-11 23:49:54 -0700

    initial

--- for/github/0-server.ccjson.js
+++ for/github/0-server.ccjson.js
@@ -2,6 +2,9 @@
 exports.forLib = function (LIB) {
     var ccjson = this;
 
+    const UUID = require("uuid");
+    const TEAM = require("./team").forLib(LIB);
+
     return LIB.Promise.resolve({
         forConfig: function (defaultConfig) {
 
@@ -10,20 +13,123 @@ exports.forLib = function (LIB) {
 
                 self.AspectInstance = function (aspectConfig) {
 
+                    var config = {};
+                    LIB._.merge(config, defaultConfig);
+                    LIB._.merge(config, instanceConfig);
+                    LIB._.merge(config, aspectConfig);
+                    config = ccjson.attachDetachedFunctions(config);
+
+                    var context = {
+                        // TODO: Instead of re-using token, issue signed requests on demand
+                        bypassToken: UUID.v4()
+                    };
+
                     return LIB.Promise.resolve({
-                        path: function () {
-
-                            var config = {};
-                            LIB._.merge(config, defaultConfig);
-                            LIB._.merge(config, instanceConfig);
-                            LIB._.merge(config, aspectConfig);
-
+                        restrictToTeam: function () {
                             return LIB.Promise.resolve(
-                                LIB.path.join(config.basePath, config.namespace || "")
+                                ccjson.makeDetachedFunction(function (credentials) {
+                                    return TEAM.isUserMember({
+                                        "teamUrl": config.teamUrl,
+                                        "token": credentials.accessToken,
+                                        "userId": credentials.id
+                                    }).then(function (isMember) {
+                                        if (isMember) {
+                                            // Success
+                                            return;
+                                        }
+                                        throw new Error("User with id '" + credentials.id + "' not found in team '" + config.teamUrl + "'");
+                                    });
+                                })
+                            );
+                        },
+                        app: function () {
+                            return LIB.Promise.resolve(
+                                ccjson.makeDetachedFunction(
+                                    function (req, res, next) {
+                                        if (
+                                            config.request &&
+                                            config.request.contextAlias
+                                        ) {
+                                            if (!req.context) {
+                                                req.context = {};
+                                            }
+                                            req.context[config.request.contextAlias] = context;
+                                        }
+
+                                        function authorize () {
+
+                                            function isAuthorized () {
+                                                // If there is a matching bypass token header we will allow the request
+                                                if (
+                                                    req.headers["x-boundary-bypass-token"] &&
+                                                    req.headers["x-boundary-bypass-token"] === context.bypassToken
+                                                ) {
+                                                    // Token matches
+                                                    return LIB.Promise.resolve(true);
+                                                }
+                                                if (
+                                                    req.context.auth &&
+                                                    req.context.auth.authorized === true
+                                                ) {
+                                                    return LIB.Promise.resolve(true);
+                                                }
+                                                return LIB.Promise.resolve(false);
+                                            }
+                                            
+                                            return isAuthorized().then(function (isAuthorized) {
+                                                if (isAuthorized) {
+                                                    return true;
+                                                }
+                                                // Use is NOT authorized. See if we have a public route.
+    
+                                                if (
+                                                    !config.public ||
+                                                    !req.context.page
+                                                ) {
+                                                    // Authorize by default if no authorizer configured
+                                                    return LIB.Promise.resolve(true);
+                                                }
+    
+                                                return req.context.page.contextForUri(req.url).then(function (pageContext) {
+                                                    function isPathAllowed () {
+                                                        if (!pageContext) return false;
+                                                        return (config.public.paths.indexOf(
+                                                            pageContext.page.lookup.path
+                                                        ) !== -1);
+                                                    }
+                                                    if (isPathAllowed()) {
+                                                        return LIB.Promise.resolve(true);
+                                                    }
+
+                                                    if (req.headers["X-Request-Type"] === "background-fetch") {
+                                                        // We are being asked to deny entry instead of redirecting.
+                                                        return LIB.Promise.resolve(false);
+                                                    }
+
+                                                    return {
+                                                        "redirectTo": pageContext.page.host.baseUrl + config.public.urls.unauthorizedRedirect
+                                                    };
+                                                });
+                                            });
+                                        }
+
+                                        return authorize().then(function (authorized) {
+                                            if (authorized !== true) {
+                                                if (authorized.redirectTo) {
+                                                    return res.redirect(authorized.redirectTo);
+                                                }
+                                                res.writeHead(403);
+                                                return res.end("Forbidden");
+                                            }
+                                            return next();
+                                        }).catch(next);
+                                    }
+                                )
                             );
                         }
                     });
                 }
+
             }
             Entity.prototype.config = defaultConfig;
 
---
+++ for/github/team.js
@@ -0,0 +1,70 @@
+
+exports.forLib = function (LIB) {
+
+    const URL = require("url");
+    const REQUEST = require("request");
+
+    var exports = {};
+
+    function callGithub(token, path) {
+        return LIB.Promise.promisify(function (callback) {
+            var url = "https://api.github.com" + path;
+            return REQUEST({
+                url: url,
+                headers: {
+                    "User-Agent": "nodejs/request",
+                    "Authorization": "token " + token
+                },
+                json: true
+            }, function (err, res, body) {
+                if (err) return callback(err);
+                if (res.statusCode === 403 || res.statusCode === 404) {
+                    console.error("Got status '" + res.statusCode + "' for url '" + url + "'! This is likely due to NOT HAVING ACCESS to this API call because your OAUTH SCOPE is too narrow! See: https://developer.github.com/v3/oauth/#scopes", res.headers);
+                    var scope = null;
+                    if (/^\/orgs\/([^\/]+)\/teams$/.test(path)) {
+                        scope = "read:org";
+                    } else
+                    if (/^\/teams\/([^\/]+)\/members\/([^\/]+)$/.test(path)) {
+                        scope = "read:org";
+                    }
+                    if (scope) {
+                        console.error("We are going to start a new oauth session with the new require scope added ...");
+                        var err = new Error("Insufficient privileges. Should start new session with added scope: " + scope);
+                        err.code = 403;
+                        err.requestScope = scope;
+                        return callback(err);
+                    }
+                    return callback(new Error("Insufficient privileges. There should be a scope upgrade handler implemented for url '" + url + "'!"));
+                }
+                return callback(null, body);
+            });
+        })();
+    }
+
+    exports.isUserMember = function (info) {
+        return LIB.Promise.try(function () {
+            var teamUrlParts = URL.parse(info.teamUrl);
+            var m = teamUrlParts.path.match(/^\/orgs\/([^\/]+)\/teams\/([^\/]+)$/);
+            if (!m) {
+                throw new Error("Invalid team URL! Copy/paste the URL from github when viewing the team.");
+            }
+            return callGithub(info.token, "/orgs/" + m[1] + "/teams").then(function (teams) {
+                var team = teams.filter(function (team) {
+                    return (team.slug === m[2]);
+                });
+                if (team.length === 0) {
+                    throw new Error("Team with slug '" + m[2] + "' not found on github!");
+                }
+                team = team.pop();
+                return callGithub(info.token, "/teams/" + team.id + "/members").then(function (members) {
+                    var member = members.filter(function (member) {
+                        return (member.id == info.userId);
+                    });
+                    return (member.length === 1);
+                });
+            });
+        });
+    }
+
+    return exports;
+}
